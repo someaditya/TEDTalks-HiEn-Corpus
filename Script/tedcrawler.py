0a744eb52d558fd973e6169342d4b525c3481495
Some Aditya Mandal <somaditya432@gmail.com>
2018-08-07 14:03:23 +0100

    Update tedcrawler.py

--- Script/tedcrawler.py
+++ Script/tedcrawler.py
@@ -56,7 +56,7 @@ def EnglishTranscript(url):
     url = url + "language=en"
     print url
     response = requests.get(url)
-    data = json.loads(response)
+    data = json.load(response)
     #print data
     print("Writing English Transcript")
     readjson(data,"eng")
@@ -68,7 +68,7 @@ def HindiTranscript(url):
     url = url + "language=hi"
     print url
     response = requests.get(url)
-    data = json.loads(response)
+    data = json.load(response)
     #print data
     print("Writing Hindi Transcript")
     readjson(data,"hi")
