689fe5f26cf34427c3440cd2065617cd81ade758
Some Aditya Mandal <somaditya432@gmail.com>
2018-12-19 15:30:56 +0000

    Update tedcrawler.py
    making the code stable

--- Script/tedcrawler.py
+++ Script/tedcrawler.py
@@ -1,5 +1,6 @@
 
 import pandas  as  pd
+import numpy as np
 from  bs4  import BeautifulSoup
 import urllib
 import codecs
@@ -18,44 +19,84 @@ sentence = ' '
 
 count = 0
 
-if os.path.exists("hindi.txt"): os.remove("hindi.txt")
-if os.path.exists("english.txt"): os.remove("english.txt")
-    
+if os.path.exists("hindi.csv"): os.remove("hindi.csv")
+if os.path.exists("english.csv"): os.remove("english.csv")
+if os.path.exists("output.csv"): os.remove("output.csv")
+
 #Web Scrapper to fetch the list of TED Talks in Hindi
 
 def List_Talks(path,list):
+
+    time.sleep(30)
     
-    r = urllib.urlopen(path).read()
-    soup = BeautifulSoup(r)
-    talks= soup.find_all("a",class_='')
-    #print(talks)
-    for i in talks:
-        if i.attrs['href'].find('/talks/')==0 :
-            talkname = i.attrs['href'].split('?')[0]
-            list.append(talkname)
+    req = requests.get(path)
+    
+    print req.status_code
+   
+    if req.status_code == 200:
+        r = urllib.urlopen(path).read()
+        soup = BeautifulSoup(r)
+        talks= soup.find_all("a",class_='')
+        for i in talks:
+            if i.attrs['href'].find('/talks/')==0 :
+                talkname = i.attrs['href'].split('?')[0]
+                list.append(talkname)
+    else:
+        time.sleep(60)
+        r = urllib.urlopen(path).read()
+        soup = BeautifulSoup(r)
+        talks= soup.find_all("a",class_='')
+        for i in talks:
+            if i.attrs['href'].find('/talks/')==0 :
+                talkname = i.attrs['href'].split('?')[0]
+                list.append(talkname)
 
     return list
 
 #Method to read the Online JSON from TED Talks Transcript URLs
 
 def readjson(data,lang):
+    
+    df = pd.DataFrame()
+    hi_text_data=[]
+    hi_time_frame=[]
+    en_text_data=[]
+    en_time_frame=[]
+
     if "paragraphs" not in data: # check if paragraphs node exits
         return
     for cues in data['paragraphs']: #iterate through paragraphs node
         for d in cues['cues']: #iterate through cues
             #print d['text']
             #To get rid of the next line error
-            oldsent =  d['text']
+            oldsent =  d['text'].encode('utf-8') 
+            timex =  str(d['time']).encode('utf-8') 
             sentence = oldsent.replace("\n", " ") 
             print(str(sentence).encode('utf-8'))
+           
             if(lang == "hi"):      
-                file = open("hindi.txt","a")
-                file.write(str(sentence).encode('utf-8')+"\n") 
+              
+                hi_text_data.append(sentence)
+                hi_time_frame.append(timex)
+
+                df1=pd.DataFrame()
+                df1[0] = hi_time_frame
+                df1[1] = hi_text_data
+   
+                df1.to_csv('hindi.csv',mode='w',index=False, header=False,sep='\t',encoding='utf-8')
+                
             else:
-                file = open("english.txt","a")
-                file.write(str(sentence).encode('utf-8')+"\n")
-
-
+                en_text_data.append(sentence)
+                en_time_frame.append(timex)
+            
+                df2=pd.DataFrame()
+                df2[0] = en_time_frame
+                df2[1] = en_text_data
+    
+                #df = pd.concat([df,df1],axis=1)
+                df2.to_csv('english.csv',mode='w', index=False, header=False,sep='\t',encoding='utf-8')
+               
+   
 #Fetch the English Transcript
 
 def EnglishTranscript(url):
@@ -70,6 +111,14 @@ def EnglishTranscript(url):
         #print data
         print("Writing English Transcript")
         readjson(data,"eng")
+    else:
+        print("Scraping too much.. Cooldown")
+        time.sleep(60) 
+        data = requests.get(url)
+        data = data.json()
+        print("Writing English Transcript")
+        readjson(data,"eng")
+
 
 #Fetch the Hindi Transcript
 
@@ -86,14 +135,25 @@ def HindiTranscript(url):
         #print data
         print("Writing Hindi Transcript")
         readjson(data,"hi")
+    else:
+        print("Scraping too much.. Cooldown")
+        time.sleep(60) 
+        data = requests.get(url)
+        data = data.json()
+        print("Writing Hindi Transcript")
+        readjson(data,"hi")
 
 all_talk_names= []
+list_of_talks=[]
 
-for i in xrange(1,13):
+for i in xrange(1,14):
     path='https://www.ted.com/talks?language=hi&page=%d'%(i)
     list_of_talks=List_Talks(path,all_talk_names)
 
+
 list_of_talks = list(set(list_of_talks))
+print("Total no of talks",len(list_of_talks))
+time.sleep(10)
 
 #list_of_talks contains the list of TED Talks in Hindi Language
 
@@ -105,23 +165,21 @@ for x in list_of_talks:
     print("Talk no: "+str(count)+" Talk name: "+"https://www.ted.com"+x)
     EnglishTranscript(url)
     HindiTranscript(url)
+
+    a = pd.read_csv("hindi.csv",sep='\t', header=None)
+    a.columns = ["time", "text"]
+    b = pd.read_csv("english.csv",sep='\t', header=None)
+    b.columns = ["time", "text"]
+
+    output = pd.merge(a,b, on='time')
+    output.to_csv('output.csv', mode='a',index=False, header=False,sep='\t',encoding='utf-8')
+
     
     print("Finished talkno "+str(count))
-    print("Sleeping for 60 seconds before htting next URL")
+    print("Sleeping for 2 seconds before htting next Talk URL")
 
     #Sleep 15/60/120/300 seconds to avoid Status Code 429 (Too Many Requests)
-    time.sleep(60) 
+    time.sleep(2) 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    import gc
+    gc.collect()
